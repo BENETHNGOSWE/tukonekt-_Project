05a8b426e3053c62c61a399746e2daf4516706a8
BENETH M. EMMANUEL <princebeneth1701@gmail.com>
2023-12-28 23:21:38 +0300

    url update4

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -12,7 +12,7 @@ class Attendance extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(Register::class);
     }
     
 }
