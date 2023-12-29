d05d790400d29493c1644912b2a3620cc96b11c1
BENETH M. EMMANUEL <princebeneth1701@gmail.com>
2023-12-29 15:55:59 +0300

    url update14

--- database/migrations/2023_11_04_132814_create_registers_table.php
+++ database/migrations/2023_11_04_132814_create_registers_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('registers', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('first_name');
             $table->string('middlename')->nullable();
             $table->string('last_name');
