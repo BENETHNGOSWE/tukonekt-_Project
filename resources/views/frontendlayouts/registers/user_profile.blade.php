0677da9f498c82370ded91116dbc52a003200362
BENETH M. EMMANUEL <princebeneth1701@gmail.com>
2024-01-09 14:33:06 +0300

    jan 9 form id update add form i on table

--- resources/views/frontendlayouts/registers/user_profile.blade.php
+++ resources/views/frontendlayouts/registers/user_profile.blade.php
@@ -1,36 +1,38 @@
- 
-<div class="container">
-        <div class="row">
-            <div class="col-md-8 offset-md-2">
-                <div class="card">
-                    <div class="card-header">
-                        User Profile
-                    </div>
-                    <div class="card-body">
-                        <table class="table">
-                            <tbody>
-                                <tr>
-                                    <th scope="row">ID</th>
-                                    <td>{{ $user->modified_id }}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">First Name</th>
-                                    <td>{{ $user->first_name }}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Last Name</th>
-                                    <td>{{ $user->last_name }}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Phone Number</th>
-                                    <td>{{ $user->phone_number }}</td>
-                                </tr>
-                                <!-- Add more fields as needed -->
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
+ <div class="container">
+     <div class="row">
+         <div class="col-md-8 offset-md-2">
+             <div class="card">
+                 <div class="card-header">
+                     User Profile
+                 </div>
+                 <div class="card-body">
+                     <table class="table">
+                         <tbody>
+                             <tr>
+                                 <th scope="row">ID</th>
+                                 <td>{{ $user->modified_id }}</td>
+                             </tr>
+                             <tr>
+                                 <th scope="row">Form ID</th> <!-- Add this row for the form_id -->
+                                 <td>{{ $user->form_id }}</td>
+                             </tr>
+                             <tr>
+                                 <th scope="row">First Name</th>
+                                 <td>{{ $user->first_name }}</td>
+                             </tr>
+                             <tr>
+                                 <th scope="row">Last Name</th>
+                                 <td>{{ $user->last_name }}</td>
+                             </tr>
+                             <tr>
+                                 <th scope="row">Phone Number</th>
+                                 <td>{{ $user->phone_number }}</td>
+                             </tr>
+                             <!-- Add more fields as needed -->
+                         </tbody>
+                     </table>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
